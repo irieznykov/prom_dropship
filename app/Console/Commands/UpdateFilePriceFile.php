8d672c348b53a573d4aed3a9db7ffbf6336fde55
Ivan Rieznykov <i.rieznykov@gmail.com>
2024-02-09 16:21:51 +0200

    logic to compare dates, get file from server

--- app/Console/Commands/UpdateFilePriceFile.php
+++ app/Console/Commands/UpdateFilePriceFile.php
@@ -6,22 +6,58 @@ namespace App\Console\Commands;
 
 use Generator;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class UpdateFilePriceFile extends Command
 {
+    protected const URL = 'http://kievopt.com.ua/prices/prom-20703.yml';
     protected $signature = 'price:update';
-
     protected $description = 'Updates the price file';
+    private string $newFilePath;
+    private string $processingFilePath;
+    private string $processedFilePath;
+    private string $datesFilePath;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->newFilePath = storage_path('app/kievoptFile.xml');
+        $this->processingFilePath = storage_path('app/kievoptFileProcessing.xml');
+        $this->processedFilePath = storage_path('app/kievoptFileProcessed.xml');
+        $this->datesFilePath = storage_path('app/dates');
+    }
 
     public function handle(): void
     {
+        Log::info('[Price update]: Started');
         $t = microtime(true);
         $memory = memory_get_peak_usage(true) / 1024 / 1024;
-        $newFile = storage_path('app/public/1234.xml');
-        $newFileStream = fopen($newFile, 'a');
+        if (!File::put($this->newFilePath, file_get_contents(self::URL))) {
+            Log::info('[Price update]: Unable to store the file from kievopt', ['url' => self::URL]);
+            return;
+        }
+        Log::info('[Price update]: New file stored');
+
+        $newDate = $this->getDate($this->newFilePath);
+
+        if (!File::exists($this->datesFilePath)) {
+            File::put($this->datesFilePath, $newDate);
+        } else {
+            $oldDate = File::get($this->datesFilePath);
+
+            if ($newDate <= $oldDate) {
+                Log::info('[Price update]: the new date isn\'t new', ['new_date' => $newDate, 'old_date' => $oldDate]);
+                return;
+            }
+        }
+
+        Log::info('[Price update]: Start processing');
+        $processingFile = fopen($this->processingFilePath, 'a');
         $i = 1;
-        foreach ($this->getItems(storage_path('app/public/123.xml')) as $el) {
+        foreach ($this->getItems($this->newFilePath) as $el) {
             if (!$el) {
                 break;
             }
@@ -49,13 +85,16 @@ class UpdateFilePriceFile extends Command
                 $el = str_replace($text, "{$text}-BS-03", $el);
             }
 
-            fwrite($newFileStream, $el);
+            fwrite($processingFile, $el);
         }
 
-        fclose($newFileStream);
+        Log::info('[Price update]: Processed');
+        if (fclose($processingFile)) {
+            File::move($this->processingFilePath, $this->processedFilePath);
+            Log::info('[Price update]: File moved');
+        }
 
-        $this->line(microtime(true) - $t);
-        $this->line($memory);
+        Log::info('[Price update]: Finished!', ['spent' => microtime(true) - $t, 'memory' => $memory]);
     }
 
     protected function getItems(string $fileName): Generator
@@ -66,5 +105,24 @@ class UpdateFilePriceFile extends Command
             }
             fclose($file);
         }
+    }
+
+    protected function getDate(string $fileName): ?string
+    {
+        $i = 0;
+        if ($file = fopen($fileName, 'r')) {
+            while (!feof($file)) {
+                $line = fgets($file);
+                if (Str::startsWith(trim($line), '<yml_catalog')) {
+                    preg_match('/<yml_catalog date=\"(.*?)\">/si', $line, $match);
+                    return $match[1] ?? null;
+                }
+                if (++$i > 3) {
+                    return null;
+                }
+            }
+        }
+
+        return null;
     }
 }
