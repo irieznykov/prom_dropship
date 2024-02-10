f6d3b25cfcc1fce9fdc3b58623e37426d9e5d213
Ivan Rieznykov <i.rieznykov@gmail.com>
2024-02-10 11:26:39 +0200

    updated schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('price:update')->everyFourHours(30);
+        $schedule->command('price:update')->everyFourHours(45);
     }
 
     protected function commands(): void
